87fa6c548f08dbbd430ebb39871e4cc97191e0c5
Abradores <gabriel.lavoro2@gmail.com>
2021-01-13 14:16:51 +0100

    continuation of the project

--- src/FeaturedQuestions.js
+++ src/FeaturedQuestions.js
@@ -1,15 +1,59 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import "./FeaturedQuestions.css"
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 function FeaturedQuestions() {
+    var today = new Date();
+    var Day = String(today.getDate()).padStart(2, '0');
+    var Month = String(today.getMonth() + 1).padStart(2, '0');
+    var Year = today.getFullYear();
+
+
+
+
+
+
+
     return (
         <div className="Featured">
             <div className="Featured__textWrapper">
-            <p className="Featured__text">Featured Questions</p>
+            Featured Questions
             </div>
+<div className="Featured__questions">
+    <div className="Featured__TotalLeft">
+    <div className="Featured__left1">
+      <Avatar alt="Anonymous"  />
+      <p>Anonymous</p>
+      <FiberManualRecordIcon style={{ color: "white" }}  fontSize="small"/>
+      <p className ="Date"> {Day} , {Month}, {Year}</p>
+      
+     <StarBorderIcon /> 
+      
+      </div>
+      <div className="Featured_left2">
+         Do you think this question will have answers?
+     </div>
+    </div>
+<div className="Featured__TotalRight">
+<div className="Featured__right1">
+      <Avatar alt="Anonymous"  />
+      <p>Anonymous</p>
+      <FiberManualRecordIcon style={{ color: "white" }} fontSize="small" />
+      <p className ="Date"> {Day} , {Month}, {Year}</p>
+      <StarBorderIcon />
+</div>
+    
 
-      <div className="Featured_left"></div>
-      <div className="Featured_right"></div>
+     <div className="Featured_right2">
+         Can this question have replays or comments?
+     </div>
+     
+
+      </div>
+</div>
+
             
         </div>
     )
