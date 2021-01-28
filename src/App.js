0abc7195f9e9b1725662e16c2d5be8ff99868f87
Abradores <gabriel.lavoro2@gmail.com>
2021-01-28 14:06:23 +0100

    started modal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import FeaturedQuestions from "./FeaturedQuestions"
 import About from "./About"
 import Creators from "./Creators"
+import Modal from "react-modal"
 
 function App() {
   return (
     <div className="App">
+      <Modal ></Modal>
    <Header />
   <FeaturedQuestions />
   <About /> 
